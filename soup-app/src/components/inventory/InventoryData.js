6d8a08e0197a84e9f765aa991f1a8d7352075cae
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-03 16:54:16 -0400

    Added initial styles to inventory cards

--- soup-app/src/components/inventory/InventoryData.js
+++ soup-app/src/components/inventory/InventoryData.js
@@ -3,28 +3,28 @@ const InventoryData = [
         item: 'Potatoes',
         unitType: 'pounds',
         quantity: 10,
-        category: 'produce',
+        category: 'Produce',
         imageURL: 'https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'
     },
     {
         item: 'Carrots',
         unitType: 'pounds',
         quantity: 3,
-        category: 'produce',
+        category: 'Produce',
         imageURL: 'https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1761&q=80'
     },
     {
         item: 'Beans',
         unitType: 'pounds',
         quantity: 5,
-        category: 'grains',
+        category: 'Grains',
         imageURL: 'https://images.unsplash.com/photo-1513868853742-e7fb786265db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80'
     },
     {
         item: 'Spaghetti',
         unitType: 'pounds',
         quantity: 3,
-        category: 'pasta',
+        category: 'Pasta',
         imageURL: 'https://images.unsplash.com/photo-1559394452-087c19c260ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80'
     } 
 ]

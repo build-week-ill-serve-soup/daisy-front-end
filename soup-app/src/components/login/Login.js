deba7a2b746b3e6e7aa4ec515fe3b6f14d0514ff
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-06 18:57:11 -0400

    Removing redux app files

--- soup-app/src/components/login/Login.js
+++ soup-app/src/components/login/Login.js
@@ -12,17 +12,28 @@ export class Login extends Component {
 
     handleChange = e => {
         this.setState({
-          credentials: {
-            ...this.state.credentials,
-            [e.target.name]: e.target.value,
-          }
+            [e.target.name]: e.target.value
         });
       }
+
+    userLogin = e => {
+        e.preventDefault();
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        axios
+        .post('https://kitchen-soup-backend.herokuapp.com/api/users/login', user)
+        .then(res => {
+            localStorage.setItem('token', res.data.token)
+            this.props.history.push('/protectedHome')
+        })
+    }
 
     render() {
         return (
             <div className="login-form">
-                <form>
+                <form onSubmit={this.userLogin}>
                     <input
                         type="text"
                         placeholder="Username"
